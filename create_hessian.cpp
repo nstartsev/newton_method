d70ebce750a7acd16be211a6d18e8358aa83bc23
nstatsev <startsev.nikolay00@gmail.com>
2023-03-20 02:25:17 +0300

    test mat

---
+++ create_hessian.cpp
@@ -0,0 +1,5 @@
+//
+// Created by мак on 20.03.2023.
+//
+
+#include "create_hessian.h"
---
+++ create_hessian.h
@@ -0,0 +1,14 @@
+//
+// Created by мак on 20.03.2023.
+//
+
+#ifndef SOLVEHESSIAN_CREATE_HESSIAN_H
+#define SOLVEHESSIAN_CREATE_HESSIAN_H
+
+
+class create_hessian {
+
+};
+
+
+#endif //SOLVEHESSIAN_CREATE_HESSIAN_H
